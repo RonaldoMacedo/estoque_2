41744ee5a6ff725242b0a63130b320adbbc44cde
RonaldoMacedo <ronni29_100@hotmail.com>
2024-12-11 09:48:26 -0300

    Projeto criado

---
+++ src/main/java/com/ronaldo/Estoque2Application.java
@@ -0,0 +1,13 @@
+package com.ronaldo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Estoque2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Estoque2Application.class, args);
+	}
+
+}
