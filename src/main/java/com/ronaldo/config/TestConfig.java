78242f546a04ae9f6f2230df148337901798dc23
RonaldoMacedo <ronni29_100@hotmail.com>
2024-12-13 12:46:20 -0300

    Datas instant padrao iso 8601

--- src/main/java/com/ronaldo/config/TestConfig.java
+++ src/main/java/com/ronaldo/config/TestConfig.java
@@ -25,12 +25,12 @@ public class TestConfig implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		Fornecedor f1 = new Fornecedor(null, "COMERCIAL 3 ALBE LTDA", "3 ALBE", "74.400.052/0001-91", null, Instant.now(), 'n');
-		Fornecedor f2 = new Fornecedor(null, "Aimara Comércio e Representações Ltda", "Aimara", "57.202.418/0001-07", null, Instant.now(), 's');
+		Fornecedor f1 = new Fornecedor(null, "COMERCIAL 3 ALBE LTDA", "3 ALBE", "74.400.052/0001-91", null, Instant.parse("2024-12-04T11:25:08Z"), 'n');
+		Fornecedor f2 = new Fornecedor(null, "Aimara Comércio e Representações Ltda", "Aimara", "57.202.418/0001-07", null, Instant.parse("2024-12-04T11:27:41Z"), 's');
 		
-		Unidade u1 = new Unidade(null, "Unidade 23 de Maio", Instant.now(), 's');
-		Unidade u2 = new Unidade(null, "Unidade Anchieta", Instant.now(), 's');
-		Unidade u3 = new Unidade(null, "Unidade Varzea Paulista", Instant.now(), 's');
+		Unidade u1 = new Unidade(null, "Unidade 23 de Maio", Instant.parse("2024-12-04T13:25:47Z"), 's');
+		Unidade u2 = new Unidade(null, "Unidade Anchieta", Instant.parse("2024-12-04T13:26:08Z"), 's');
+		Unidade u3 = new Unidade(null, "Unidade Varzea Paulista", Instant.parse("2024-12-04T13:29:15Z"), 's');
 		
 		fornecedorRepository.saveAll(Arrays.asList(f1, f2));
 		unidadeRepository.saveAll(Arrays.asList(u1, u2, u3));
